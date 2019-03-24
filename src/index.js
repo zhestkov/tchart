e9458a265d077497d1cbfddb9db5f48c71bd322e
Andrey Zhestkov <ajestkov@gmail.com>
2019-03-24 23:35:01 +0300

    fix styles

--- src/index.js
+++ src/index.js
@@ -20,23 +20,23 @@ const months = [
 const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 document.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("night");
   const container = document.getElementById("container");
   container.classList.add("container");
   const charts = document.createElement("div");
   charts.classList.add("charts");
-  document.body.appendChild(container);
   container.appendChild(charts);
 
   const nightButton = createElement("button");
-  nightButton.innerText = "Switch to Night Mode";
-  nightButton.classList.add("night-button");
+  nightButton.innerText = "Switch to Day Mode";
+  nightButton.classList.add("theme-toggle");
   nightButton.addEventListener("click", () => {
     document.body.classList.toggle("night");
 
     if (document.body.classList.contains("night")) {
       nightButton.innerText = "Switch to Day Mode";
     } else {
-      nightButton.innerText = "Switch to Dark Mode";
+      nightButton.innerText = "Switch to Night Mode";
     }
   });
 
